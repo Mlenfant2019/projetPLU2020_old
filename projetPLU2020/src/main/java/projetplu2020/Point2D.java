607db2b286d6978d5800e149412bf2674eccf846
maxim <maxim@DESKTOP-44PPBCL>
2020-09-14 15:15:43 +0200

    completion de la classe

--- projetPLU2020/src/main/java/projetplu2020/Point2D.java
+++ projetPLU2020/src/main/java/projetplu2020/Point2D.java
@@ -30,4 +30,25 @@ public class Point2D {
     public int getY(){
         return this.y;
     }
+    public void setX(int x){
+        this.x = x;
+    }
+    public void setY(int y){
+        this.y = y;
+    }
+    public void setPosition(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+    public void translate(int a,int b){
+        this.x = this.getX() + a;
+        this.y = this.getY() + b;
+    }
+    @Override
+    public String toString(){
+        return "[" + this.getX() + ";" + this.getY() + "]";
+    }
+    public void affiche(){
+        System.out.println(this.toString());
+    }
 }
